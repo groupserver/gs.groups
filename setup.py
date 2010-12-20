a40001b1ccc6e35c0c57f26c41887298fb36f181
Richard Waid <richard@iopen.net>
2010-12-20 14:27:52 +1300

    Add new version.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(name='gs.groups',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['gs'],
     include_package_data=True,
-    zip_safe=False,
+    zip_safe=True,
     install_requires=[
         'setuptools',
         # -*- Extra requirements: -*-
