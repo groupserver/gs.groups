6b34a32a7ef3458429bc1ee6c1eaac353abea200
Michael JasonSmith <mpj17@onlinegroups.net>
2012-06-08 10:19:02 +1000

    List all the Private groups on a site.

--- gs/groups/sitehomeviewlet.py
+++ gs/groups/sitehomeviewlet.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
 from gs.viewlet.viewlet import SiteViewlet
+from private import PrivateGroups
 
 class GroupsList(SiteViewlet):
-    pass
+    @Lazy
+    def privateGroups(self):
+        return PrivateGroups(self.context)
