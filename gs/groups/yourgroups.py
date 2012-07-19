fc9dd2432fe6f9f9b92ef4d969f26f2339909df9
mpj17@onlinegroups.net <mpj17@onlinegroups.net>
2012-07-19 17:22:25 +1200

    YourGroups added

---
+++ gs/groups/yourgroups.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+from zope.cachedescriptors.property import Lazy
+from zope.component import createObject
+from gs.group.privacy.visibility import GroupVisibility
+from gs.group.member.base.utils import user_member_of_group
+from allgroups import AllGroupsOnSite
+
+class YourGroups(AllGroupsOnSite):
+    # --=mpj17=-- should this be moved to gs.group.member.base?
+
+    @Lazy
+    def loggedInUser(self):
+        retval = createObject('groupserver.LoggedInUser', self.context)
+        return retval
+
+    @Lazy
+    def groups(self):
+        retval = [g for g in self.get_all_groups() 
+                  if user_member_of_group(self.loggedInUser, g)]
+        return retval
