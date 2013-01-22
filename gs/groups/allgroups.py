28964f0d0813fb85e79c2b826cf61b34ee4636cc
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-22 11:10:07 +1100

    General code cleanup, thanks to Ninja IDE.

--- gs/groups/allgroups.py
+++ gs/groups/allgroups.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
-from zope.interface.common.mapping import IEnumerableMapping 
+from zope.interface.common.mapping import IEnumerableMapping
 from zope.interface import implements
-from zope.schema.interfaces import IVocabulary, IVocabularyTokenized, \
-    ITitledTokenizedTerm
+from zope.schema.interfaces import IVocabulary, IVocabularyTokenized
 from zope.schema.vocabulary import SimpleTerm
 from gs.group.base.interfaces import IGSGroupMarker
 
-FOLDER_TYPES =  ('Folder', 'Folder (Ordered)')
+FOLDER_TYPES = ('Folder', 'Folder (Ordered)')
+
 
 # --=mpj17=-- This deliberately has an awkward name so people are less
 # likely to use it
@@ -18,7 +18,7 @@ class AllGroupsOnSite(object):
 
     @Lazy
     def siteInfo(self):
-        retval = createObject('groupserver.SiteInfo', self.context)        
+        retval = createObject('groupserver.SiteInfo', self.context)
         return retval
 
     @Lazy
@@ -27,24 +27,24 @@ class AllGroupsOnSite(object):
         assert retval, 'Cannot find "groups" folder in %s' % \
             self.siteInfo.siteObj
         return retval
-    
+
     def get_all_groups(self):
         retval = [createObject('groupserver.GroupInfo', g)
                   for g in self.groupsFolder.objectValues(FOLDER_TYPES)
                   if IGSGroupMarker.providedBy(g)]
-        retval.sort(key=lambda g:g.name.lower())
+        retval.sort(key=lambda g: g.name.lower())
         return retval
 
     @Lazy
     def groups(self):
         return self.get_all_groups()
-    
+
     @Lazy
     def groupIds(self):
         retval = [g.id for g in self.groups]
         assert len(retval) == len(self.groups)
         return retval
-    
+
     @Lazy
     def groupDict(self):
         retval = dict([(g.id, g) for g in self.groups])
@@ -60,10 +60,11 @@ class AllGroupsOnSite(object):
         assert group, 'No group given'
         retval = ((group in self.groupIds) or (group in self.groups))
         return retval
-    
+
     def __getitem__(self, key):
         retval = self.groupDict[key]
         return retval
+
 
 class AllGroupsOnSiteVocab(AllGroupsOnSite):
     implements(IVocabulary, IVocabularyTokenized)
@@ -79,8 +80,8 @@ class AllGroupsOnSiteVocab(AllGroupsOnSite):
         return retval
 
     def __contains__(self, groupId):
-        assert group, 'No groupId given'
-        retval = (group in self.groupIds)
+        assert groupId, 'No groupId given'
+        retval = (groupId in self.groupIds)
         return retval
 
     @Lazy
@@ -91,9 +92,9 @@ class AllGroupsOnSiteVocab(AllGroupsOnSite):
     def __getitem__(self, key):
         retval = self.termDict[key]
         return retval
-    
+
     def getTerm(self, value):
         return self[value]
-    
+
     def getTermByToken(self, value):
         return self[value]
--- gs/groups/interfaces.py
+++ gs/groups/interfaces.py
@@ -4,29 +4,34 @@ from zope.interface.interface import Interface
 from zope.schema import List, Text
 from zope.viewlet.interfaces import IViewletManager
 
+
 class IGSGroupsInfo(Interface):
-    def get_visible_group_ids(): #@NoSelf
+    def get_visible_group_ids():  # lint:ok
         """Get the IDs of Visible Groups
-        
+
         ARGUMENTS
             None.
-            
+
         RETURNS
             A list of strings, representing the IDs of visible groups.
         """
 
+
 class IGSGroupsFolder(Interface):
     pass
 
+
 class IGroupList(IViewletManager):
     '''The lists of groups on the site.'''
+
 
 class IGroupListContentProvider(IContentProvider):
     groups = List(title=u'Groups',
                   description=u'The groups to list.',
                   required=True)
-    pageTemplateFileName = Text(title=u"Page Template File Name",
-                                description=u'The name of the ZPT file that '\
-                                    u'is used to render the list.',
-                                required=False,
-                                default=u"browser/templates/groupslistcontentprovider.pt")
+    pageTemplateFileName = Text(
+        title=u"Page Template File Name",
+        description=u'The name of the ZPT file that  is used to render the '
+                    u'list.',
+        required=False,
+        default=u"browser/templates/groupslistcontentprovider.pt")
