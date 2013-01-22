c41aacc08d4eb04d1a1bc019dce84733094b4c07
Michael JasonSmith <mpj17@onlinegroups.net>
2013-01-22 11:10:53 +1100

    Code cleanup, thanks to Ninja IDE, and making the get_all_groups method easier to debug.

--- gs/groups/groupsInfo.py
+++ gs/groups/groupsInfo.py
@@ -1,79 +1,83 @@
-from zope.interface import implements, implementedBy
-from zope.component import adapts
+# coding=utf-8
+import time
 from zope.app.folder.interfaces import IFolder
+from zope.cachedescriptors.property import Lazy
+from zope.component import adapts
 from zope.component.interfaces import IFactory
-
+from zope.interface import implements, implementedBy
 import AccessControl
-
 from Products.GSGroup.queries import GroupQuery
 from Products.XWFCore.cache import LRUCache
-from Products.XWFCore.XWFUtils import get_group_by_siteId_and_groupId
-from Products.CustomUserFolder.interfaces import ICustomUser
 from Products.GSContent.interfaces import IGSSiteInfo
 from gs.groups.interfaces import IGSGroupsInfo
 
-import time
 import logging
 log = logging.getLogger('GSGroupsInfo')
 
+
 class GSGroupsInfoFactory(object):
     implements(IFactory)
-    
+
     title = u'GroupServer Groups Info Factory'
     descripton = u'Create a new GroupServer groups information instance'
-    
+
     def __call__(self, context):
         retval = GSGroupsInfo(context)
         return retval
-        
+
     def getInterfaces(self):
         retval = implementedBy(GSGroupsInfo)
         assert retval
         return retval
 
+
 class GSGroupsInfo(object):
-    implements( IGSGroupsInfo )
+    implements(IGSGroupsInfo)
     adapts(IFolder)
 
     siteUserVisibleGroupsIds = LRUCache("siteUserVisibleGroupIds")
     siteUserVisibleGroupsIds.set_max_objects(256)
 
     def __init__(self, context):
-        self.context=context
+        self.context = context
         self.siteInfo = IGSSiteInfo(context)
-        self.groupsObj = self.__get_groups_object()
-        
         self.folderTypes = ['Folder', 'Folder (Ordered)']
-
         self.groupQuery = GroupQuery(context)
         self.__allGroups = None
-                
-    def __get_groups_object(self):
+
+    @Lazy
+    def groupsObj(self):
         assert self.siteInfo, 'Site Info is set to %s' % self.siteInfo
         assert self.siteInfo.siteObj, \
           'Site Object is %s' % self.siteInfo.siteObj
-        
+
         assert hasattr(self.siteInfo.siteObj, 'groups'), \
           'Site "%s" has not "groups" instance within "Content"' % \
           self.siteInfo.get_name()
         groupsObj = getattr(self.siteInfo.siteObj, 'groups')
-        
-        assert groupsObj.getProperty('is_groups' , False), \
+
+        assert groupsObj.getProperty('is_groups', False), \
           'Groups instance for "%s" exists, but the "is_groups" property '\
           'is not True' % self.siteInfo.get_name()
-        
+
         assert groupsObj
         return groupsObj
 
+    @Lazy
+    def allFolderIds(self):
+        retval = self.groupsObj.objectIds(self.folderTypes)
+        return retval
+
     def get_all_groups(self):
-        assert self.groupsObj
-        if self.__allGroups == None:
-            folders = self.groupsObj.objectValues(self.folderTypes)
-            self.__allGroups = [g for g in folders
-                                if g.getProperty('is_group', False)]
+        if self.__allGroups is None:
+            self.__allGroups = []
+            for folderId in self.allFolderIds:
+                folder = getattr(self.groupsObj, folderId)
+                if folder.getProperty('is_group', False):
+                    self.__allGroups.append(folder)
             self.__allGroups.sort(groups_sorter)
+            assert type(self.__allGroups) == list
         retval = self.__allGroups
-        assert type(retval) == list
         return retval
 
     def get_visible_groups(self):
@@ -86,7 +90,7 @@ class GSGroupsInfo(object):
         groups = '-'.join(user.getGroups())
         key = '-'.join((top_level_site_id, self.siteInfo.id, groups))
 
-        if self.siteUserVisibleGroupsIds.has_key(key):
+        if self.siteUserVisibleGroupsIds.has_key(key):  # lint:ok
             visibleGroupsIds = self.siteUserVisibleGroupsIds.get(key)
             visibleGroups = []
             for groupId in visibleGroupsIds:
@@ -100,15 +104,14 @@ class GSGroupsInfo(object):
             visibleGroupsIds = [group.getId() for group in visibleGroups]
             self.siteUserVisibleGroupsIds.add(key, visibleGroupsIds)
             bottom = time.time()
-            log.debug("Generated visible-groups for (%s) on %s (%s) in %.2fms" % 
-                      (userId, self.siteInfo.name, self.siteInfo.id, (bottom-top)*1000.0))        
-            
-        assert self.siteUserVisibleGroupsIds.has_key(key)
-        assert type(visibleGroups) == list
-        
+            log.debug("Generated visible-groups for (%s) on %s (%s) in "
+                        "%.2fms" % (userId, self.siteInfo.name,
+                                    self.siteInfo.id, (bottom - top) * 1000.0))
 
+        assert self.siteUserVisibleGroupsIds.has_key(key)  # lint:ok
+        assert type(visibleGroups) == list
         return visibleGroups
-        
+
     def __visible_groups_for_current_user(self):
         securityManager = AccessControl.getSecurityManager()
         allGroups = self.get_all_groups()
@@ -119,21 +122,22 @@ class GSGroupsInfo(object):
             # AM: "Visible groups" should really be: groups which a user
             #   is a member of, public groups, and private groups. Therefore,
             #   we should only be checking the visibility of the group, not
-            #   of the messages. 
+            #   of the messages.
             #   A separate method ("visible messages" or similar) should be
             #   used to determine what messages and files should be included
-            #   in search results (and in turn, latest topics and files on a 
+            #   in search results (and in turn, latest topics and files on a
             #   site homepage) should be shown to users.
-            #   **HOWEVER** at this point in time, we do not make a distinction.
-            #   Therefore, to preserve security, we define "visible groups"
-            #   very restrictively.
+            #   **HOWEVER** at this point in time, we do not make a
+            #   distinction. Therefore, to preserve security, we define
+            #   "visible groups" very restrictively.
             if (hasattr(group, 'messages')
               and securityManager.checkPermission('View', group)
-              and securityManager.checkPermission('View', group.aq_explicit.messages)):
+              and securityManager.checkPermission('View',
+                                                  group.aq_explicit.messages)):
                 visibleGroups.append(group)
         assert type(visibleGroups) == list
         return visibleGroups
-        
+
     def get_visible_group_ids(self):
         retval = [g.getId() for g in self.get_visible_groups()]
         return retval
@@ -150,7 +154,7 @@ class GSGroupsInfo(object):
 
     def get_non_member_groups_for_user(self, user):
         '''List the visible groups that the user is not a member of.
-        
+
         WARNING
             Listing the groups that the user is *not* a member of is
             as bad as listing the groups that the user *is* a member of.
@@ -164,7 +168,7 @@ class GSGroupsInfo(object):
                   if ('GroupMember' not in user.getRolesInContext(g))]
         assert type(retval) == list
         return retval
-        
+
     def get_joinable_groups_for_user(self, user):
         assert user
         listManager = self.context.ListManager
@@ -183,19 +187,19 @@ class GSGroupsInfo(object):
 
     def get_member_groups_for_user(self, user, authUsr=None):
         """Get a list of all groups that the user is a member of.
-        
+
         ARGUMENTS
             user: A user instance to query.
-            authenticatedUser: The instance of the user who is requesting 
+            authenticatedUser: The instance of the user who is requesting
                 the information.
-        
+
         RETURNS
             A list of groups the user is a member of. If "user" is the
             same as the "authenticatedUser", then the list will be of all
             groups that "user" is a member of. Otherwise only the groups
-            that the user is a member of, and has posted to, will be 
+            that the user is a member of, and has posted to, will be
             listed.
-        
+
         SIDE EFFECTS
             None.
         """
@@ -204,24 +208,24 @@ class GSGroupsInfo(object):
         if (authUsr and (user.getId() == authUsr.getId())):
             retval = self.__get_member_groups_for_user(user)
         else:
-            retval = self.__get_visible_member_groups_for_user(user,authUsr)
+            retval = self.__get_visible_member_groups_for_user(user, authUsr)
         assert type(retval) == list
         return retval
 
     def __get_member_groups_for_user(self, user):
         """Get a list of all groups the user is currently a member of.
-        
+
         WARNING
             Showing group-memberships to the wrong people may result in
-            a privacy breach. It is safer to use 
+            a privacy breach. It is safer to use
             "get_member_groups_for_user"
-            
+
         ARGUMENTS
             user: A user instance.
-            
+
         RETURNS
             A list of all groups the user is currently a member of.
-        
+
         SIDE EFFECTS
             None.
         """
@@ -236,15 +240,15 @@ class GSGroupsInfo(object):
         """Get the publicly visible groups that the user is a member of,
            and the private groups that the viewing user (authUsr) is
            also a member of.
-        
+
         ARGUMENTS
             user: A user instance.
             authUsr: The authenticated user (may be None)
-            
+
         RETURNS
             A list of groups that the user is currently a member of, and
             has posted to. (Unless
-        
+
         SIDE EFFECTS
             None.
         """
@@ -269,6 +273,7 @@ class GSGroupsInfo(object):
         assert type(retval) == list
         return retval
 
+
 def groups_sorter(a, b):
     at = a.title_or_id().lower()
     bt = b.title_or_id().lower()
@@ -284,4 +289,3 @@ def groups_sorter(a, b):
     else:
         retval = 1
     return retval
-
