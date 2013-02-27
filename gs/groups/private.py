98f7b3a76e1b1e01ef2e851373a0c6be54ee7d5e
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-27 17:28:00 +1100

    Code cleanup.

--- gs/groups/private.py
+++ gs/groups/private.py
@@ -1,14 +1,14 @@
 # coding=utf-8
 from zope.cachedescriptors.property import Lazy
-from zope.component import createObject
 from gs.group.privacy.visibility import GroupVisibility
 from allgroups import AllGroupsOnSite
+
 
 class PrivateGroups(AllGroupsOnSite):
 
     @Lazy
     def groups(self):
-        retval = [g for g in self.get_all_groups() 
+        retval = [g for g in self.get_all_groups()
                   if GroupVisibility(g).isPrivate]
         return retval
 
