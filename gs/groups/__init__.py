e6003fdbba8ba40cd628e56e6db2c987dd7d9a2c
Michael JasonSmith <mpj17@onlinegroups.net>
2013-02-27 17:33:37 +1100

    Removing some vocabs, that are not used.

--- gs/groups/__init__.py
+++ gs/groups/__init__.py
@@ -6,8 +6,3 @@ groupsInfo_security = ModuleSecurityInfo('gs.groups.groupsInfo')
 from groupsInfo import GSGroupsInfoFactory, GSGroupsInfo
 allow_class(GSGroupsInfoFactory)
 allow_class(GSGroupsInfo)
-
-# --=mpj17=-- Do not import allgroups, because it is too general
-from public import PublicGroups
-from private import PrivateGroups
-from secret import SecretGroups
