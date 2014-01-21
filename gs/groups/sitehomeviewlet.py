75942d7cff55b7ebd7798c944ea32b2697a5e4e2
Michael JasonSmith <mpj17@onlinegroups.net>
2014-01-20 17:16:02 -0800

    Adding copyright blocks, and switching to Absolute Import

--- gs/groups/sitehomeviewlet.py
+++ gs/groups/sitehomeviewlet.py
@@ -1,8 +1,22 @@
-# coding=utf-8
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# Copyright © 2014 OnlineGroups.net and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+from __future__ import absolute_import
 from zope.cachedescriptors.property import Lazy
 from gs.viewlet.viewlet import SiteViewlet
-from allgroups import AllGroupsOnSite
-from membergroups import PublicGroups, PrivateGroups, SecretGroups
+from .allgroups import AllGroupsOnSite
+from .membergroups import PublicGroups, PrivateGroups, SecretGroups
 
 
 class ListVisible(SiteViewlet):
