b96fe6323e48e3e708c75d164314eab4d29c53bd
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-12 09:35:14 -0800

    Adding Copyright blocks.

--- gs/groups/__init__.py
+++ gs/groups/__init__.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
 from AccessControl import ModuleSecurityInfo
 from AccessControl import allow_class
 
 groupsInfo_security = ModuleSecurityInfo('gs.groups.groupsInfo')
-from groupsInfo import GSGroupsInfoFactory, GSGroupsInfo
+from .groupsInfo import GSGroupsInfoFactory, GSGroupsInfo
 allow_class(GSGroupsInfoFactory)
 allow_class(GSGroupsInfo)
